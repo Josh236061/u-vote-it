f9e51f12a348184bce89a4548a9de78c5383b179
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-25 18:11:08 -0500

    Updated server.js Database Calls

--- server.js
+++ server.js
@@ -26,6 +26,34 @@ const db = mysql.createConnection(
     console.log(rows);
   });
 
+  // GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+/* Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+}); */
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
