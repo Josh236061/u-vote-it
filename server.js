07bd21a2f0e2e412f678918f4f142b456f3ff8f0
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-26 16:28:53 -0500

    Finalized Lesson 3 code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password 
-  password: '',
+  password: 'Greenlantern1990!',
   database: 'election'
 });
 
