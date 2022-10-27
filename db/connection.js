4a4fbd576baef74ea06247a738ce0831f901af04
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-10-27 17:22:25 -0500

    Added modularization to Application 'candidates' & 'parties'

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password 
+    password: 'Greenlantern1990!',
+    database: 'election'
+  });
+
+
+
+  module.exports = db;
